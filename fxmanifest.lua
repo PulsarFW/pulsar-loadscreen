94ea523e5ba7f5ba1ad34c871a9060d8689fdf77
BaskinB <54458253+BaskinB@users.noreply.github.com>
2026-05-02 11:03:52 -0500

    feat(server): added version checking.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,11 @@
 fx_version "cerulean"
 game 'gta5'
+lua54 'yes'
 description "Pulsar Loading Screen"
 author "Pulsar"
+version '1.0.2'
+
+server_script 'version.lua'
 
 loadscreen 'web/build/index.html'
 loadscreen_manual_shutdown 'yes'
@@ -10,5 +14,3 @@ files {
   'web/build/index.html',
   'web/build/**/*',
 }
-
-lua54 'yes'
